35e1b2edbf7d47c7987b6a5a78e67196c4c19edb
Jesse Wolfhagen <jl.wolfhagen@gmail.com>
2025-08-29 13:18:37 -0400

    Updating Bayesian model code
    Updating code to fix issue between "rstan" and "cmdstanr" with workaround function, also updating .stan files to new array syntax

--- Scripts/d18O Model Example Script.R
+++ Scripts/d18O Model Example Script.R
@@ -1,55 +1,29 @@
-library("data.table")
+####Import libraries####
+#packages for Bayesian analysis
+library("bayesplot")
 library("cmdstanr")
-library("rstan")
+#packages for data analysis/structuring
+library("data.table")
+#packages for visualization and publishing
+library("circular")
+library("ggdist")
+library("ggforce")
 library("ggplot2")
-#
-blanz_etal_data <- fread("./Data/Blanz, et al. 2020 Sheep CO Data (Cleaned).csv")
-rousay_data <- blanz_etal_data[Site %in% "ROU"]
-#
-rousay_data[, Specimen_No := as.numeric(as.factor(Specimen))]
-rousay_data[, Sample_No := 1:.N, Specimen_No]
 
-rousay_standata <- list(
-  N_Specimens = rousay_data[!is.na(d18O), .N, Specimen_No][, .N],
-  N_Samples = rousay_data[!is.na(d18O), .N],
-  Specimen = rousay_data[!is.na(d18O), as.numeric(as.factor(Specimen_No))],
-  #observations
-  dist_obs = rousay_data[!is.na(d18O), Dist],
-  oxy_obs = rousay_data[!is.na(d18O), d18O],
-  dist_sd = rep(0.5, rousay_data[!is.na(d18O), .N]),
-  oxy_sd = rousay_data[!is.na(d18O), O.Error],
-  #hyper-parameters
-  prior_period = c(30, 3.5),
-  period_low = 10,
-  period_high = 70
-)
-oxy_slope_stanmodel <- cmdstan_model("./Scripts/d18O_multilevel_slopemodel.stan")
-rousay_samples <- oxy_slope_stanmodel$sample(
-  data = rousay_standata,
-  chains = 4,
-  parallel_chains = 4,
-  refresh = 250,
-  adapt_delta = 0.95,
-  max_treedepth = 15
-)
+####Local functions####
+#Function to extract posterior samples from a {cmdstan} model fit
+#replaces lost functionality of using {rstan} extract() function
+cmdstanr_extract_samples <- function(fit_obj) {
+  #credit to Andrew Johnson for this code (source: https://discourse.mc-stan.org/t/rstan-read-stan-csv-throwing-error-with-cmdstan-models-versions-2-35/35665/7)
+  vars <- fit_obj$metadata()$stan_variables
+  draws <- posterior::as_draws_rvars(fit_obj$draws())
+  
+  lapply(vars, \(var_name){  
+    posterior::draws_of(draws[[var_name]], with_chains = FALSE)
+  }) |> setNames(vars)
+}
 
-rousay_samples$summary(c("mu_period_oxy", "mu_alpha_oxy", "mu_beta_oxy", "mu_Obeta1", "mu_Obeta2", "sigma_specimen", "mu_mean_oxy", "mu_amplitude_oxy"))
-rousay_samples$summary(c("period_oxy", "alpha_oxy", "beta_oxy", "Obeta1", "Obeta2"))
-rousay_samples$summary("sigmasample_O")
-#
-rousay_samples$summary(c("sampled_period_oxy", "sampled_alpha_oxy", "sampled_beta_oxy"))
-rousay_samples$summary(c("mu_max_oxy", "sampled_max_oxy"))
-
-rousay_stanfit <- rstan::read_stan_csv(rousay_samples$output_files())
-
-#Visualizing some of the model fit using traceplots and pair plots
-traceplot(rousay_stanfit, pars = "alpha_oxy")
-pairs(rousay_stanfit, pars = c("mu_alpha_oxy", "mu_beta_oxy", "mu_Obeta1", "mu_Obeta2", "sigmasample_O", "lp__"))
-
-#extracting the posterior samples
-rousay_post <- extract(rousay_stanfit)
-
-####Functions####
+#Functions for plotting isotopic profiles
 #This function calculates the sinusoidal curve for a series of parameters and distance values
 oxyfx <- function(params, dist) {
   period <- params[1]
@@ -60,7 +34,7 @@ oxyfx <- function(params, dist) {
   (intercept + slope * dist) + (beta1 * cos(2 * pi * (dist / period))) + (beta2 * sin(2 * pi * (dist / period)))
 }
 
-#adds confidence intervals for observed d18O values
+#This function adds confidence intervals to d18O observations
 add_d18O_CIs <- function(data, posterior, specimen_no, col = "black") {
   #collect which observations from the posterior model to include
   observations <- which(data[, Specimen_No] %in% specimen_no)
@@ -68,7 +42,7 @@ add_d18O_CIs <- function(data, posterior, specimen_no, col = "black") {
   lapply(1:CI_data[, .N], function(x) geom_segment(data = CI_data[x], aes(x = Dist, xend = Dist, y = CI_low, yend = CI_high), col = col))
 }
 
-#adds isotopic profile curves (with uncertainty) and linear trend
+#This function adds
 add_isotopic_profile <- function(posterior, specimen_no, col = "black", linear_trend = T) {
   isotopic_profile_df <- rbindlist(lapply(1:nrow(posterior$alpha_oxy), function(x) data.table(Iteration = x, Dist = seq(40, 0, length.out = 1000), m_d18O = oxyfx(params = c(posterior$period_oxy[x, specimen_no], posterior$alpha_oxy[x, specimen_no], posterior$beta_oxy[x, specimen_no], posterior$Obeta1[x, specimen_no], posterior$Obeta2[x, specimen_no]), dist = seq(40, 0, length.out = 1000)))))
   linear_trend_df <- rbindlist(lapply(1:nrow(posterior$alpha_oxy), function(x) data.table(Iteration = x, Dist = seq(40, 0, length.out = 1000), m_d18O_line = posterior$alpha_oxy[x, specimen_no] + posterior$beta_oxy[x, specimen_no] * seq(40, 0, length.out = 1000))))
@@ -105,28 +79,9 @@ add_site_isotopic_profile <- function(posterior, col = "black") {
   )
 }
 
-#These functions are used to create estimates of the relative position of curve peaks
-#(relative to the periodicity), which is used as a relative estimate of birth seasonality.
-#If values are spread out across an assemblage, then the birth seasonality was wider.
+#Functions for the circular plot (relative position of the max point)
 
-#Calculates the quantiles (default is 95%) of the relative position of the maximum value
-#This is necessary because these could range across the 0 point in circular data
-get_quantile_arc <- function(theta_vals, ci = 0.95, n = 1000) {
-  #data are set up with theta (in radians)
-  quantile_vals <- as.numeric(quantile(circular::circular(theta_vals), c((1 - ci) / 2, 1 - ((1 - ci) / 2))))
-  #create the range of theta values, but first check if the values span the 0 point
-  if(quantile_vals[1] <= quantile_vals[2]) {
-    #doesn't span 0
-    arc_vals <- seq(quantile_vals[1], quantile_vals[2], length.out = n)
-  }
-  if(quantile_vals[1] > quantile_vals[2]) {
-    #spans 0
-    arc_vals <- seq(quantile_vals[1], quantile_vals[2] + 2 * pi, length.out = n)
-  }
-  arc_vals
-}
-
-#Adds quantile curve of a specific quantile length to a circular plot
+#This function calculates the limits of the critical point in an arc of a unit circle
 max_oxy_quantiles <- function(theta_vals, ci = 0.95) {
   #data are set up with theta (in radians)
   quantile_vals <- as.numeric(quantile(circular::circular(theta_vals), c((1 - ci) / 2, 1 - ((1 - ci) / 2))))
@@ -142,8 +97,7 @@ max_oxy_quantiles <- function(theta_vals, ci = 0.95) {
   arc_vals
 }
 
-
-#Adds 80% and 95% quantiles (along with median point) of the relative position of a curve's maximum in circular form
+#This function adds the arcs to an existing ggplot
 add_max_oxy_line <- function(data, radius, col = "black") {
   plot_data <- rbindlist(lapply(c(0.95, 0.80), function(x) max_oxy_quantiles(data, ci = x)))
   plot_median <- data.table(Median = as.numeric(median(circular::circular(data))))
@@ -157,10 +111,10 @@ add_max_oxy_line <- function(data, radius, col = "black") {
 #Calculates the location of all of the curves (and average values) for an assemblage to help with labeling
 plot_data_creator <- function(data, posterior) {
   #collect summary information on each specimen
-  plot_data <- rbindlist(lapply(1:data[, .N, Specimen_No][, .N], function(x) data.table(Specimen_No = x, Specimen = data[, .N, .(Specimen_No, Specimen)][order(Specimen_No), Specimen][x], Median = as.numeric(median(circular::circular(posterior$max_oxy[, x]))), max_oxy_quantiles(posterior$max_oxy[, x], ci = 0.95))))
+  plot_data <- rbindlist(lapply(1:data[, .N, Specimen_No][, .N], function(x) data.table(Specimen_No = x, Specimen = data[, .N, .(Specimen_No, Specimen)][order(Specimen_No), Specimen][x], Median = as.numeric(median(circular::circular(posterior$max_oxy_radian[, x]))), max_oxy_quantiles(posterior$max_oxy_radian[, x], ci = 0.95))))
   #add in the overall mean
   plot_data <- rbind(plot_data,
-                     data.table(Specimen_No = (data[, .N, Specimen_No][, .N] + 1), Specimen = "Herd Average", Median = as.numeric(median(circular::circular(posterior$mu_max_oxy))), max_oxy_quantiles(posterior$mu_max_oxy, ci = 0.95)))
+                     data.table(Specimen_No = (data[, .N, Specimen_No][, .N] + 1), Specimen = "Herd Average", Median = as.numeric(median(circular::circular(posterior$mu_max_oxy_radian))), max_oxy_quantiles(posterior$mu_max_oxy_radian, ci = 0.95)))
   
   #convert median values to x/y to organize plot, as if radius = 1 (have to kind of go by quandrant--goal is to go from closest-to-zero [vertical] to furthest-from-zero [which wraps all the way around, awkwardly, to vertical])
   plot_data[, c("Median_X", "Median_Y") := .(1 * sin(Median), 1 * cos(Median))]
@@ -186,11 +140,57 @@ plot_data_creator <- function(data, posterior) {
   list(Summary = plot_data, Label_Plotting = plot_data_with_points)
 }
 
+####Import the isotopic data####
+blanz_etal_data <- fread("./Data/Blanz, et al. 2020 Sheep CO Data (Cleaned).csv")
+rousay_data <- blanz_etal_data[Site %in% "ROU"]
+#
+rousay_data[, Specimen_No := as.numeric(as.factor(Specimen))]
+rousay_data[, Sample_No := 1:.N, Specimen_No]
+
+rousay_standata <- list(
+  N_Specimens = rousay_data[!is.na(d18O), .N, Specimen_No][, .N],
+  N_Samples = rousay_data[!is.na(d18O), .N],
+  Specimen = rousay_data[!is.na(d18O), as.numeric(as.factor(Specimen_No))],
+  #observations
+  dist_obs = rousay_data[!is.na(d18O), Dist],
+  oxy_obs = rousay_data[!is.na(d18O), d18O],
+  dist_sd = rep(0.5, rousay_data[!is.na(d18O), .N]),
+  oxy_sd = rousay_data[!is.na(d18O), O.Error],
+  #hyper-parameters
+  prior_period = c(30, 3.5),
+  period_low = 10,
+  period_high = 70
+)
+oxy_slope_stanmodel <- cmdstan_model("./Scripts/d18O_multilevel_slopemodel.stan")
+rousay_samples <- oxy_slope_stanmodel$sample(
+  data = rousay_standata,
+  chains = 4,
+  parallel_chains = 4,
+  refresh = 250,
+  adapt_delta = 0.95,
+  max_treedepth = 15
+)
+
+rousay_samples$summary(c("mu_period_oxy", "mu_alpha_oxy", "mu_beta_oxy", "mu_Obeta1", "mu_Obeta2", "sigma_specimen", "mu_mean_oxy", "mu_amplitude_oxy"))
+rousay_samples$summary(c("period_oxy", "alpha_oxy", "beta_oxy", "Obeta1", "Obeta2"))
+rousay_samples$summary("sigmasample_O")
+#
+rousay_samples$summary(c("sampled_period_oxy", "sampled_alpha_oxy", "sampled_beta_oxy"))
+rousay_samples$summary(c("mu_max_oxy_d18O", "sampled_max_oxy_d18O"))
+
+#Visualizing some of the model fit using traceplots and pair plots
+mcmc_trace(rousay_samples$draws(), regex_pars = "alpha_oxy")
+mcmc_pairs(rousay_samples$draws(), regex_pars = c("mu_alpha_oxy", "mu_beta_oxy", "mu_Obeta1", "mu_Obeta2", "sigmasample_O", "lp__"))
+
+#extracting the posterior samples
+rousay_post <- cmdstanr_extract_samples(rousay_samples)
+
 #Plotting the isotopic profile of a single specimen####
 specimen_no <- 2
 title_message <- paste0("Rousay Sheep M2: ", rousay_data[, .N, .(Specimen_No, Specimen)][Specimen_No %in% specimen_no, Specimen])
 #
-ggplot(rousay_data[Specimen_No %in% specimen_no]) +
+indiv_profile_plot <- 
+  ggplot(rousay_data[Specimen_No %in% specimen_no]) +
   add_isotopic_profile(posterior = rousay_post, specimen_no = specimen_no, col = "black") +
   geom_point(aes(x = Dist, y = d18O), col = "black") +
   add_d18O_CIs(data = rousay_data, posterior = rousay_post$d18O, specimen_no = specimen_no, col = "black") +
@@ -199,38 +199,41 @@ ggplot(rousay_data[Specimen_No %in% specimen_no]) +
   scale_alpha_manual(name = "", values = c("Isotopic Profile" = 0.3, "Linear Trend" = 0.1), labels = c("Isotopic Profile", "Linear Trend")) +
   labs(title = title_message, x = "Distance from REJ (mm)", y = expression(paste(delta^{18}, "O (\u2030)"))) +
   theme_classic() + theme(panel.border = element_rect(fill = NA), legend.text = element_text(size = 10), legend.position = "bottom", legend.justification = c("center"), legend.box.just = "right", legend.margin = margin(6, 6, 6, 6), plot.title = element_text(size = 16), plot.subtitle = element_text(size = 12), axis.title = element_text(size = 12), axis.text = element_text(size = 10))
-
+indiv_profile_plot
 #Average profile for the whole assemblage####
 site_name <- "Rousay"
-ggplot(rousay_data[Specimen_No %in% specimen_no, .(Dist, d18O = fourth_d18O)][!is.na(d18O)]) +
+site_avg_plot <- 
+  ggplot(rousay_data[Specimen_No %in% specimen_no, .(Dist, d18O = d18O)][!is.na(d18O)]) +
   add_site_isotopic_profile(posterior = rousay_post, col = "black") +
   coord_cartesian(xlim = c(40, 0), ylim = c(-7, -1), expand = F) +
   scale_linetype_manual(name = "", values = c("Isotopic Profile" = "solid", "Linear Trend" = "dotted"), labels = c("Isotopic Profile", "Linear Trend")) +
   scale_alpha_manual(name = "", values = c("Isotopic Profile" = 0.3, "Linear Trend" = 0.1), labels = c("Isotopic Profile", "Linear Trend")) +
   labs(title = paste(site_name, "(Average Profile)", sep = " "), x = "Distance from REJ (mm)", y = expression(paste(delta^{18}, "O (\u2030)"))) +
   theme_classic() + theme(panel.border = element_rect(fill = NA), legend.text = element_text(size = 10), legend.position = "bottom", legend.justification = c("center"), legend.box.just = "right", legend.margin = margin(6, 6, 6, 6), plot.title = element_text(size = 16), plot.subtitle = element_text(size = 12), axis.title = element_text(size = 12), axis.text = element_text(size = 10))
-
+site_avg_plot
 #Seasonality estimate for every sample with average####
 full_plot_data <- plot_data_creator(data = rousay_data, posterior = rousay_post)
-ggplot() +
+site_seasonality_plot <-
+  ggplot() +
   geom_arc(aes(x0 = 0, y0 = 0, r = 1, start = 0, end = 2 * pi), linetype = "dotted") +
   geom_segment(aes(x = -1, xend = 1, y = 0, yend = 0), linetype = "dotted") +
   geom_segment(aes(x = 0, xend = 0, y = -1, yend = 1), linetype = "dotted") +
-  sapply(1:10, function(x) add_max_oxy_line(data = rousay_post$max_oxy[, x], radius = seq(0.75, 1.25, 0.05)[full_plot_data$Summary[order(Specimen_No), Median_Rank]][x], col = "grey20")) +
-  add_max_oxy_line(data = rousay_post$mu_max_oxy, radius = seq(0.75, 1.25, 0.05)[full_plot_data$Summary[order(Specimen_No), Median_Rank]][11], col = "black") +
-  #  geom_text_repel(data = full_plot_data$Label_Plotting[is.na(Specimen_No) | (Specimen_No %in% 1:10)], aes(x = Radius * sin(Max), y = Radius * cos(Max), label = Specimen), max.overlaps = Inf, min.segment.length = 0, size = 3) +
+  sapply(1:10, function(x) add_max_oxy_line(data = rousay_post$max_oxy_radian[, x], radius = seq(0.75, 1.25, 0.05)[full_plot_data$Summary[order(Specimen_No), Median_Rank]][x], col = "grey")) +
+  add_max_oxy_line(data = rousay_post$mu_max_oxy_radian, radius = seq(0.75, 1.25, 0.05)[full_plot_data$Summary[order(Specimen_No), Median_Rank]][11], col = "black") +
   geom_text_repel(data = full_plot_data$Label_Plotting[is.na(Specimen_No) | (Specimen_No %in% 11)], aes(x = Radius * sin(Min), y = Radius * cos(Min), label = Specimen), max.overlaps = Inf, col = "black", min.segment.length = 0) +
-  labs(title = "Estimated Birth Seasonality: Rousay Sheep M2", x = "", y = "") +
-  scale_color_manual(labels = c("Specimen", "Herd Average"), values = c("grey", "black")) +
-  theme_classic() + theme(legend.text = element_text(size = 10), legend.position = "bottom", legend.justification = c("center"), legend.box.just = "right", legend.margin = margin(6, 6, 6, 6), axis.title = element_blank(), axis.text = element_blank())
+  labs(title = "Estimated Birth Seasonality", x = "", y = "") +
+  theme_classic() + theme(legend.text = element_text(size = 8), legend.position = "bottom", legend.justification = c("center"), legend.box.just = "right", legend.margin = margin(6, 6, 6, 6), plot.title = element_text(size = 10), plot.title.position = "plot", plot.subtitle = element_text(size = 8), axis.title = element_text(size = 8), axis.text = element_text(size = 8))
+site_seasonality_plot
 
 #Average seasonality estimate (and expected range) for the site####
-ggplot() +
+site_avg_seasonality_plot <- 
+  ggplot() +
   geom_arc(aes(x0 = 0, y0 = 0, r = 1, start = 0, end = 2 * pi), linetype = "dotted") +
   geom_segment(aes(x = -1, xend = 1, y = 0, yend = 0), linetype = "dotted") +
   geom_segment(aes(x = 0, xend = 0, y = -1, yend = 1), linetype = "dotted") +
-  add_max_oxy_line(data = rousay_post$mu_max_oxy, radius = 0.95, col = "black") +
-  add_max_oxy_line(data = rousay_post$sampled_max_oxy, radius = 1.05, col = "blue") +
-  geom_arc_bar(data = max_oxy_quantiles(rousay_post$mu_max_oxy, ci = 0.95), aes(x0 = 0, y0 = 0, r0 = 0, r = 1.05, start = Min, end = Max), alpha = 0.05, fill = "black", col = "black", linetype = "dashed") +
+  add_max_oxy_line(data = rousay_post$mu_max_oxy_radian, radius = 0.95, col = "black") +
+  add_max_oxy_line(data = rousay_post$sampled_max_oxy_radian, radius = 1.05, col = "blue") +
+  geom_arc_bar(data = max_oxy_quantiles(rousay_post$mu_max_oxy_radian, ci = 0.95), aes(x0 = 0, y0 = 0, r0 = 0, r = 1.05, start = Min, end = Max), alpha = 0.05, fill = "black", col = "black", linetype = "dashed") +
   labs(title = "Herd Average (Black) and Range (Blue)", subtitle = site_name, x = "", y = "") +
   theme_classic() + theme(legend.text = element_text(size = 10), legend.position = "bottom", legend.justification = c("center"), legend.box.just = "right", legend.margin = margin(6, 6, 6, 6), axis.title = element_blank(), axis.text = element_blank())
+site_avg_seasonality_plot
--- Scripts/d18O_multilevel_slopemodel.stan
+++ Scripts/d18O_multilevel_slopemodel.stan
@@ -1,14 +1,14 @@
 data {
     int<lower = 1> N_Specimens;
     int<lower = 1> N_Samples;
-    int<lower = 0> Specimen[N_Samples];
+    array[N_Samples] int<lower = 0> Specimen;
     //observations
-    real<lower = 0> dist_obs[N_Samples];
-    real oxy_obs[N_Samples];
-    real<lower = 0> dist_sd[N_Samples];
-    real<lower = 0> oxy_sd[N_Samples];
+    array[N_Samples] real<lower = 0> dist_obs;
+    array[N_Samples] real oxy_obs;
+    array[N_Samples] real<lower = 0> dist_sd;
+    array[N_Samples] real<lower = 0> oxy_sd;
     //hyper-parameters
-    real<lower = 0> prior_period[2];
+    array[2] real<lower = 0> prior_period;
     real<lower = 0> period_low;
     real<lower = period_low> period_high;
 }
@@ -58,7 +58,7 @@ model {
     mu_Obeta1 ~ normal(0, 1.5);
     mu_Obeta2 ~ normal(0, 1.5);
     //variation between specimens
-    sigma_specimen[1] ~ normal(0, 0.25); //bounded_period_oxy
+    sigma_specimen[1] ~ normal(0, 0.5); //bounded_period_oxy
     sigma_specimen[2] ~ normal(0, 1); //alpha_oxy
     sigma_specimen[3] ~ normal(0, 0.01); //beta_oxy
     sigma_specimen[4:5] ~ normal(0, 1); //Obeta1, Obeta2
@@ -82,11 +82,21 @@ generated quantities {
   //herd-level parameters
   real mu_mean_oxy;
   real mu_amplitude_oxy;
-  real mu_max_oxy;
+  real mu_max_oxy_dist;
+  real mu_min_oxy_dist;
+  real mu_max_oxy_radian;
+  real mu_max_oxy_d18O; //d18O value at max_oxy
+  real mu_min_oxy_d18O; //d18O value at min_oxy point (max_oxy + 0.5 * periodicity)
+  real mu_mid_oxy_d18O; //d18O value at mid-poitn between max_oxy and min_oxy (max_oxy + 0.25 * periodicity)
   //specimen-level parameters
   vector[N_Specimens] amplitude_oxy;
   vector[N_Specimens] mean_oxy;
-  vector[N_Specimens] max_oxy;
+  vector[N_Specimens] max_oxy_dist;
+  vector[N_Specimens] min_oxy_dist;
+  vector[N_Specimens] max_oxy_radian;
+  vector[N_Specimens] max_oxy_d18O; //d18O value at max_oxy
+  vector[N_Specimens] min_oxy_d18O; //d18O value at min_oxy point (max_oxy + 0.5 * periodicity)
+  vector[N_Specimens] mid_oxy_d18O; //d18O value at mid-poitn between max_oxy and min_oxy (max_oxy + 0.25 * periodicity)
   //sampling the parameters to get a set of uncertainty about the population
   matrix[5, 1] z_sampled;
   matrix[1, 5] v_sampled;
@@ -95,39 +105,65 @@ generated quantities {
   real sampled_beta_oxy;
   real sampled_Obeta1;
   real sampled_Obeta2;
-  real sampled_max_oxy;
-  //herd-level parameters for average values (mean_oxy, amplitude_oxy, max_oxy)
+  real sampled_max_oxy_dist;
+  real sampled_min_oxy_dist;
+  real sampled_max_oxy_radian;
+  real sampled_max_oxy_d18O;
+  real sampled_min_oxy_d18O;
+  real sampled_mid_oxy_d18O;
+  
+  //herd-level parameters for average values (mean_oxy, amplitude_oxy, max_oxy_dist, min_oxy_dist, max_oxy_radian)
   mu_mean_oxy = ((mu_alpha_oxy + (mu_beta_oxy * mu_period_oxy)) + (mu_alpha_oxy + (mu_beta_oxy * 0))) / 2;
   mu_amplitude_oxy = sqrt(pow(mu_Obeta1, 2) + pow(mu_Obeta2, 2));
   if(mu_Obeta2 / mu_Obeta1 > 0 && mu_Obeta2 > 0) {
-    mu_max_oxy = (((atan(mu_Obeta2 / mu_Obeta1) * (mu_period_oxy / (2 * pi()))) + 0) / mu_period_oxy) * 2 * pi();
+    mu_max_oxy_dist = (atan(mu_Obeta2 / mu_Obeta1) * (mu_period_oxy / (2 * pi()))) + 0;
+    mu_min_oxy_dist = mu_max_oxy_dist + (mu_period_oxy / 2);
   }
   if(mu_Obeta2 / mu_Obeta1 > 0 && mu_Obeta2 <= 0) {
-    mu_max_oxy = (((atan(mu_Obeta2 / mu_Obeta1) * (mu_period_oxy / (2 * pi()))) + (mu_period_oxy / 2)) / mu_period_oxy) * 2 * pi();
+    mu_max_oxy_dist = (atan(mu_Obeta2 / mu_Obeta1) * (mu_period_oxy / (2 * pi()))) + (mu_period_oxy / 2);
+    mu_min_oxy_dist = mu_max_oxy_dist - (mu_period_oxy / 2);
   }
   if(mu_Obeta2 / mu_Obeta1 <= 0 && mu_Obeta2 > 0) {
-    mu_max_oxy = (((atan(mu_Obeta2 / mu_Obeta1) * (mu_period_oxy / (2 * pi()))) + (mu_period_oxy / 2)) / mu_period_oxy) * 2 * pi();
+    mu_max_oxy_dist = (atan(mu_Obeta2 / mu_Obeta1) * (mu_period_oxy / (2 * pi()))) + (mu_period_oxy / 2);
+    mu_min_oxy_dist = mu_max_oxy_dist + (mu_period_oxy / 2);
   }
   if(mu_Obeta2 / mu_Obeta1 <= 0 && mu_Obeta2 <= 0) {
-    mu_max_oxy = (((atan(mu_Obeta2 / mu_Obeta1) * (mu_period_oxy / (2 * pi()))) + (mu_period_oxy)) / mu_period_oxy) * 2 * pi();
+    mu_max_oxy_dist = (atan(mu_Obeta2 / mu_Obeta1) * (mu_period_oxy / (2 * pi()))) + (mu_period_oxy);
+    mu_min_oxy_dist = mu_max_oxy_dist - (mu_period_oxy / 2);
   }
-  //specimen-level parameters (mean_oxy, amplitude_oxy, max_oxy)
+  mu_max_oxy_radian = (mu_max_oxy_dist / mu_period_oxy) * 2 * pi();
+  //calculations for curve-related d18O values (max_d18O, min_d180, mean_d18O)
+  mu_max_oxy_d18O = mu_alpha_oxy + (mu_beta_oxy * mu_max_oxy_dist) + mu_Obeta1 * cos(2 * pi() * (mu_max_oxy_dist / mu_period_oxy)) + mu_Obeta2 * sin(2 * pi() * (mu_max_oxy_dist / mu_period_oxy));
+  mu_min_oxy_d18O = mu_alpha_oxy + (mu_beta_oxy * mu_min_oxy_dist) + mu_Obeta1 * cos(2 * pi() * (mu_min_oxy_dist / mu_period_oxy)) + mu_Obeta2 * sin(2 * pi() * (mu_min_oxy_dist / mu_period_oxy));
+  mu_mid_oxy_d18O = mu_alpha_oxy + (mu_beta_oxy * (mu_max_oxy_dist + mu_min_oxy_dist) / 2);
+
+  //specimen-level parameters (mean_oxy, amplitude_oxy, max_oxy_dist, min_oxy_dist, max_oxy_radian)
   for(i in 1:N_Specimens) {
     mean_oxy[i] = ((alpha_oxy[i] + beta_oxy[i] * period_oxy[i]) + (alpha_oxy[i] + beta_oxy[i] * 0)) / 2;
     amplitude_oxy[i] = sqrt(pow(Obeta1[i], 2) + pow(Obeta2[i], 2));
     if(Obeta2[i] / Obeta1[i] > 0 && Obeta2[i] > 0) {
-      max_oxy[i] = (((atan(Obeta2[i] / Obeta1[i]) * (period_oxy[i] / (2 * pi()))) + 0) / period_oxy[i]) * 2 * pi();
+      max_oxy_dist[i] = (atan(Obeta2[i] / Obeta1[i]) * (period_oxy[i] / (2 * pi()))) + 0;
+      min_oxy_dist[i] = max_oxy_dist[i] + (period_oxy[i] / 2);
     }
     if(Obeta2[i] / Obeta1[i] > 0 && Obeta2[i] <= 0) {
-      max_oxy[i] = (((atan(Obeta2[i] / Obeta1[i]) * (period_oxy[i] / (2 * pi()))) + (period_oxy[i] / 2)) / period_oxy[i]) * 2 * pi();
+      max_oxy_dist[i] = (atan(Obeta2[i] / Obeta1[i]) * (period_oxy[i] / (2 * pi()))) + (period_oxy[i] / 2);
+      min_oxy_dist[i] = max_oxy_dist[i] - (period_oxy[i] / 2);
     }
     if(Obeta2[i] / Obeta1[i] <= 0 && Obeta2[i] > 0) {
-      max_oxy[i] = (((atan(Obeta2[i] / Obeta1[i]) * (period_oxy[i] / (2 * pi()))) + (period_oxy[i] / 2)) / period_oxy[i]) * 2 * pi();
+      max_oxy_dist[i] = (atan(Obeta2[i] / Obeta1[i]) * (period_oxy[i] / (2 * pi()))) + (period_oxy[i] / 2);
+      min_oxy_dist[i] = max_oxy_dist[i] + (period_oxy[i] / 2);
     }
     if(Obeta2[i] / Obeta1[i] <= 0 && Obeta2[i] <= 0) {
-      max_oxy[i] = (((atan(Obeta2[i] / Obeta1[i]) * (period_oxy[i] / (2 * pi()))) + (period_oxy[i])) / period_oxy[i]) * 2 * pi();
+      max_oxy_dist[i] = (atan(Obeta2[i] / Obeta1[i]) * (period_oxy[i] / (2 * pi()))) + (period_oxy[i]);
+      min_oxy_dist[i] = max_oxy_dist[i] - (period_oxy[i] / 2);
     }
+    max_oxy_radian[i] = (max_oxy_dist[i] / period_oxy[i]) * 2 * pi();
+    //specimen-level calculations for curve-related d18O values (max_d18O, min_d180, mean_d18O)
+    max_oxy_d18O[i] = alpha_oxy[i] + (beta_oxy[i] * max_oxy_dist[i]) + Obeta1[i] * cos(2 * pi() * (max_oxy_dist[i] / period_oxy[i])) + Obeta2[i] * sin(2 * pi() * (max_oxy_dist[i] / period_oxy[i]));
+    min_oxy_d18O[i] = alpha_oxy[i] + (beta_oxy[i] * min_oxy_dist[i]) + Obeta1[i] * cos(2 * pi() * (min_oxy_dist[i] / period_oxy[i])) + Obeta2[i] * sin(2 * pi() * (min_oxy_dist[i] / period_oxy[i]));
+    mid_oxy_d18O[i] = alpha_oxy[i] + (beta_oxy[i] * (max_oxy_dist[i] + min_oxy_dist[i]) / 2);
   }
+  
   //values for a sampled specimen (uncertainty: sampled_max_oxy)
   for(i in 1:5) {
     z_sampled[i, 1] = normal_rng(0, 1);
@@ -140,15 +176,23 @@ generated quantities {
   sampled_Obeta2 = mu_Obeta2 + v_sampled[1, 5];
   //calculate the sampled max_oxy value
   if(sampled_Obeta2 / sampled_Obeta1 > 0 && sampled_Obeta2 > 0) {
-    sampled_max_oxy = (((atan(sampled_Obeta2 / sampled_Obeta1) * (sampled_period_oxy / (2 * pi()))) + 0) / sampled_period_oxy) * 2 * pi();
+    sampled_max_oxy_dist = (atan(sampled_Obeta2 / sampled_Obeta1) * (sampled_period_oxy / (2 * pi()))) + 0;
+    sampled_min_oxy_dist = sampled_max_oxy_dist + (sampled_period_oxy / 2);
   }
   if(sampled_Obeta2 / sampled_Obeta1 > 0 && sampled_Obeta2 <= 0) {
-    sampled_max_oxy = (((atan(sampled_Obeta2 / sampled_Obeta1) * (sampled_period_oxy / (2 * pi()))) + (sampled_period_oxy / 2)) / sampled_period_oxy) * 2 * pi();
+    sampled_max_oxy_dist = (atan(sampled_Obeta2 / sampled_Obeta1) * (sampled_period_oxy / (2 * pi()))) + (sampled_period_oxy / 2);
+    sampled_min_oxy_dist = sampled_max_oxy_dist - (sampled_period_oxy / 2);
   }
   if(sampled_Obeta2 / sampled_Obeta1 <= 0 && sampled_Obeta2 > 0) {
-    sampled_max_oxy = (((atan(sampled_Obeta2 / sampled_Obeta1) * (sampled_period_oxy / (2 * pi()))) + (sampled_period_oxy / 2)) / sampled_period_oxy) * 2 * pi();
+    sampled_max_oxy_dist = (atan(sampled_Obeta2 / sampled_Obeta1) * (sampled_period_oxy / (2 * pi()))) + (sampled_period_oxy / 2);
+    sampled_min_oxy_dist = sampled_max_oxy_dist + (sampled_period_oxy / 2);
   }
   if(sampled_Obeta2 / sampled_Obeta1 <= 0 && sampled_Obeta2 <= 0) {
-    sampled_max_oxy = (((atan(sampled_Obeta2 / sampled_Obeta1) * (sampled_period_oxy / (2 * pi()))) + (sampled_period_oxy)) / sampled_period_oxy) * 2 * pi();
+    sampled_max_oxy_dist = (atan(sampled_Obeta2 / sampled_Obeta1) * (sampled_period_oxy / (2 * pi()))) + (sampled_period_oxy);
+    sampled_min_oxy_dist = sampled_max_oxy_dist - (sampled_period_oxy / 2);
   }
+  sampled_max_oxy_radian = (sampled_max_oxy_dist / sampled_period_oxy) * 2 * pi();
+  sampled_max_oxy_d18O = sampled_alpha_oxy + (sampled_beta_oxy * sampled_max_oxy_dist) + sampled_Obeta1 * cos(2 * pi() * (sampled_max_oxy_dist / sampled_period_oxy)) + sampled_Obeta2 * sin(2 * pi() * (sampled_max_oxy_dist / sampled_period_oxy));
+  sampled_min_oxy_d18O = sampled_alpha_oxy + (sampled_beta_oxy * sampled_min_oxy_dist) + sampled_Obeta1 * cos(2 * pi() * (sampled_min_oxy_dist / sampled_period_oxy)) + sampled_Obeta2 * sin(2 * pi() * (sampled_min_oxy_dist / sampled_period_oxy));
+  sampled_mid_oxy_d18O = sampled_alpha_oxy + (sampled_beta_oxy * (sampled_max_oxy_dist + sampled_min_oxy_dist) / 2);
 }
